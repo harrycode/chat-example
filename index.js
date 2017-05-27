f8933a9e852b14a1163966819d0039e38fdba557
harrycode <pacboy2004@gmail.com>
2017-05-27 19:42:13 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var io = require("socket.io");
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+var socket = io.listen(3000, "82.46.42.157");
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+socket.set("log level", 1);
+var people = {};
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+socket.on("connection", function (client) {
+
+	client.on("join", function(name){
+		people[client.id] = name;
+		client.emit("update", "You have connected to the server.");
+		socket.sockets.emit("update", name + " has joined the server.")
+		socket.sockets.emit("update-people", people);
+	});
+
+	client.on("send", function(msg){
+		socket.sockets.emit("chat", people[client.id], msg);
+	});
+
+	client.on("disconnect", function(){
+		socket.sockets.emit("update", people[client.id] + " has left the server.");
+		delete people[client.id];
+		socket.sockets.emit("update-people", people);
+	});
 });
